866081f6902a3fe2315033252a1abdaa2f9a2b3b
Danijel <glisovic.danijel96@gmail.com>
2024-06-10 13:58:01 +0200

    Added more integration tests.

--- CustomRedis/RedisLite.Test/ServerTests.cs
+++ CustomRedis/RedisLite.Test/ServerTests.cs
@@ -1,12 +1,12 @@
 ﻿using Newtonsoft.Json;
 using RedisLite.Server;
-using System.Net.Http.Json;
 using System.Text;
 
 namespace RedisLite.Test;
 
 public class ServerTests
 {
+    private readonly string _rediLiteAddress = $"http://{_host}:{_port}/";
     private readonly HttpClient client = new HttpClient();
     private LiteHttpServer redisLiteHttpServer;
     private const string _host = "127.0.0.1";
@@ -22,16 +22,94 @@ public class ServerTests
     }
 
     [Fact]
-    public async void OnSendingPingCommad_Passes_ReceivePongResponse()
+    public async void OnSendingPingCommand_Passes_ReceivePongResponse()
     {
-        var stringPayload = JsonConvert.SerializeObject(new { message = "*1\r\n$4\r\nPING\r\n"});
+        var httpContent = CreateStringContent("*1\r\n$4\r\nPING\r\n");
 
-        var httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
-
-        var response = await client.PostAsync($"http://{_host}:{_port}/", httpContent);
+        var response = await client.PostAsync(_rediLiteAddress, httpContent);
 
         var responseString = await response.Content.ReadAsStringAsync();
 
         Assert.Equal("+PONG\r\n", responseString);
+    }
+
+    [Fact]
+    public async void OnSendingEchoCommand_Passes_ReceiveCorrectResponse()
+    {
+        var httpContent = CreateStringContent("*2\r\n$4\r\nECHO\r\n$11\r\nHello world\r\n");
+
+        var response = await client.PostAsync(_rediLiteAddress, httpContent);
+
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        Assert.Equal("+Hello world\r\n", responseString);
+    }
+
+    [Fact]
+    public async void OnSendingGetCommand_Passes_ReceiveCorrectResponseWhenTheValueDoesNotExist()
+    {
+        var httpContent = CreateStringContent("*2\r\n$3\r\nget\r\n$15\r\nNameNotExisting\r\n");
+
+        var response = await client.PostAsync(_rediLiteAddress, httpContent);
+
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        Assert.Equal("$-1\r\n", responseString);
+    }
+
+    [Fact]
+    public async void OnSendingSetCommand_Passes_TheValueIsSaved()
+    {
+        var httpContent = CreateStringContent("*3\r\n$3\r\nset\r\n$7\r\nSetName\r\n$4\r\nBrad\r\n");
+
+        var response = await client.PostAsync(_rediLiteAddress, httpContent);
+
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        Assert.Equal("+OK\r\n", responseString);
+    }
+
+    [Fact]
+    public async void OnSendingSetCommand_Passes_TheValueExistsAndIsUpdated()
+    {
+        var httpContentForCreate = CreateStringContent("*3\r\n$3\r\nset\r\n$7\r\nSetName\r\n$4\r\nBrad\r\n");
+        var httpContentForUpdate = CreateStringContent("*3\r\n$3\r\nset\r\n$7\r\nSetName\r\n$4\r\nLuca\r\n");
+        var httpContentForGet = CreateStringContent("*2\r\n$3\r\nget\r\n$7\r\nSetName\r\n");
+
+        var createResponse = await client.PostAsync(_rediLiteAddress, httpContentForCreate);
+        var createResponseString = await createResponse.Content.ReadAsStringAsync();
+
+        var updateResponse = await client.PostAsync(_rediLiteAddress, httpContentForUpdate);
+        var updateResponseString = await updateResponse.Content.ReadAsStringAsync();
+
+        var getResponse = await client.PostAsync(_rediLiteAddress, httpContentForGet);
+        var getResponseString = await getResponse.Content.ReadAsStringAsync();
+
+        Assert.Equal("+OK\r\n", createResponseString);
+        Assert.Equal("+OK\r\n", updateResponseString);
+        Assert.Equal("$4\r\nLuca\r\n", getResponseString);
+    }
+
+    [Fact]
+    public async void OnSendingGetCommand_Passes_ReceiveCorrectResponseWhenTheValueExists()
+    {
+        var httpContentCreate = CreateStringContent("*3\r\n$3\r\nset\r\n$4\r\nTest\r\n$4\r\nBrad\r\n");
+        var httpContentGet = CreateStringContent("*2\r\n$3\r\nget\r\n$4\r\nTest\r\n");
+
+        var createResponse = await client.PostAsync(_rediLiteAddress, httpContentCreate);
+        var createResponseString = await createResponse.Content.ReadAsStringAsync();
+
+        var getResponse = await client.PostAsync(_rediLiteAddress, httpContentGet);
+        var getResponseString = await getResponse.Content.ReadAsStringAsync();
+
+        Assert.Equal("+OK\r\n", createResponseString);
+        Assert.Equal("$4\r\nBrad\r\n", getResponseString);
+    }
+
+    private StringContent CreateStringContent(string message)
+    {
+        var stringPayload = JsonConvert.SerializeObject(new { message });
+
+        return new StringContent(stringPayload, Encoding.UTF8, "application/json");
     }
 }
